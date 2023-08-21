9933c2f673b84f295db2202fa04e525a4066add3
chaane <hlalelechaane@gmail.com>
2023-08-21 19:18:25 +0200

    chaane

--- project_AI.py
+++ project_AI.py
@@ -12,7 +12,7 @@ import wikipedia
 import wolframalpha
 
 #configure browser
-
+#QQQAAALLAA
 chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 webbrowser.register('chrome',None,webbrowser.BackgroundBrowser(chrome_path))
 
