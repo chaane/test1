66e559634d3272854782c8f2a147b1c7240c97bd
chaane <hlalelechaane@gmail.com>
2023-08-21 19:55:45 +0200

    Removed the comment

--- project_AI.py
+++ project_AI.py
@@ -10,10 +10,6 @@ import pyttsx3
 import speech_recognition as sr
 import wikipedia
 import wolframalpha
-
-#cNOU HUZET HIER
-
-#QQQAAALLAA
 chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 webbrowser.register('chrome',None,webbrowser.BackgroundBrowser(chrome_path))
 
